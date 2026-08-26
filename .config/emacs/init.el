c4831098856eee96916c1a1239cbd9167bbf1345
ldlpiao <51768815+LDLPiao@users.noreply.github.com>
2026-08-26 12:18:19 -0300

    backup pré lsp-mode

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,9 +1,14 @@
+;;; -*- lexical-binding: t -*-
 ;; Define o caminho relativo usando a pasta base do Emacs
 (add-to-list 'custom-theme-load-path (expand-file-name "themes/" user-emacs-directory))
 
 ;; Carrega o tema noctalia
 (load-theme 'noctalia t)
 
+(add-to-list 'default-frame-alist
+             '(font . "JetBrainsMono Nerd Font 14"))
+
+;; (set-fontset-font "fontset-default" nil "Symbols Nerd Font" nil 'append)
 
 ;; Desabilita barras de rolagem e ferramentas
 (tool-bar-mode -1)
@@ -11,14 +16,27 @@
 ;; Habilita os números das linhas
 (global-display-line-numbers-mode 1)
 ;; Define a opacidade do fundo do frame
-(set-frame-parameter nil 'alpha-background 80)
+(set-frame-parameter nil 'alpha-background 60)
 
 (setq history-length 25)
+(setq make-backup-files nil)
 (savehist-mode 1)
 
 (recentf-mode 1)
 (save-place-mode 1)
 (global-auto-revert-mode 1)
+
+;; Configura e organiza os backups do emacs
+; Source - https://stackoverflow.com/a/151946
+; Posted by jfm3, modified by community. See post 'Timeline' for change history
+; Retrieved 2026-08-23, License - CC BY-SA 3.0
+(setq backup-directory-alist `(("." . "~/.saves")))
+(setq version-control t     ;; Use version numbers for backups.
+      kept-new-versions 10  ;; Number of newest versions to keep.
+      kept-old-versions 0   ;; Number of oldest versions to keep.
+      delete-old-versions t ;; Don't ask to delete excess backup versions.
+      backup-by-copying t)  ;; Copy all files, don't rename them.
+(setq vc-make-backup-files t) ;; Backup versioned files
 
 (setq use-dialog-box nil)
 
@@ -29,7 +47,8 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
- 
+
+;; Navegação vim-like com evil-mode
 (use-package evil
   :ensure t
   :init
@@ -49,10 +68,36 @@
   :config
   (global-evil-surround-mode 1))
 
+(use-package undo-tree
+  :ensure t
+  :init
+  (global-undo-tree-mode)
+  :config
+  (evil-set-undo-system 'undo-tree))
+
+;; Configuração de syntax highlighting
 (use-package treesit-auto
   :ensure t
   :custom
   (treesit-auto-install 'prompt)
   :config
-  (treesit-auto-add-to-auto-mode-alist '(lua))
+  (treesit-auto-add-to-auto-mode-alist
+   '(lua
+     pytho))
   (global-treesit-auto-mode))
+
+(use-package fish-mode
+  :ensure t
+  :mode ("\\.fish\\'" . fish-mode))
+
+;; Ativa lsp-mode
+
+
+;; Emulador de terminal dentro do emacs
+(use-package vterm
+  :ensure t
+  :commands vterm
+  :custom
+  (vterm-always-compile-module t)
+  :bind
+  ("<f7>" . vterm))
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,3 +12,22 @@ starship init fish | source
 function config
 	 git --git-dir=$HOME/.dotfiles --work-tree=$HOME $argv
 end
+
+function vterm_printf;
+    if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end
+        # tell tmux to pass the escape sequences through
+        printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+    else if string match -q -- "screen*" "$TERM"
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$argv"
+    else
+        printf "\e]%s\e\\" "$argv"
+    end
+end
+
+if [ "$INSIDE_EMACS" = 'vterm' ]
+    function clear
+        vterm_printf "51;Evterm-clear-scrollback";
+        tput clear;
+    end
+end
